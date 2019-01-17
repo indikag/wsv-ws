31ade68a23d7e18c0f2e8834f654eb22d6149802
Indika Gunawardana <indika.gunawardana@gmail.com>
2019-01-18 04:58:22 +0530

    Initial file

---
+++ src/main/java/com/jsv/rest/util/Log.java
@@ -0,0 +1,13 @@
+package com.jsv.rest.util;
+/*
+ * User: Indika Gunawardana
+ * Date: 1/18/19
+ * Time: 4:56 AM
+ * Copyright(c) 2018 AXIS, LLC.
+ */
+
+public class Log {
+    public static void log(String message) {
+        System.out.println("====LOG====" + message);
+    }
+}
