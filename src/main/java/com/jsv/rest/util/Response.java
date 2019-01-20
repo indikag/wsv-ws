764b9adf6538e59842c235259f445bcba35ce8e4
Indika Gunawardana <indika.gunawardana@gmail.com>
2019-01-21 04:09:28 +0530

    Initial file

---
+++ src/main/java/com/jsv/rest/util/Response.java
@@ -0,0 +1,37 @@
+package com.jsv.rest.util;
+/*
+ * User: Indika Gunawardana
+ * Date: 1/21/19
+ * Time: 4:03 AM
+ * Copyright(c) 2018 AXIS, LLC.
+ */
+
+public class Response {
+    private Status status;
+    private Object payload;
+
+    public Response(Status status, Object payload) {
+        this.status = status;
+        this.payload = payload;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Object getPayload() {
+        return payload;
+    }
+
+    public void setPayload(Object payload) {
+        this.payload = payload;
+    }
+
+    @Override public String toString() {
+        return "Response{" + "status=" + status + ", payload=" + payload + '}';
+    }
+}
