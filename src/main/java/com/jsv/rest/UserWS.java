3e6f62b2199b0f4421a6e0422e32af5540176b53
Indika Gunawardana <indika.gunawardana@gmail.com>
2019-01-21 03:46:17 +0530

    Added login service

--- src/main/java/com/jsv/rest/UserWS.java
+++ src/main/java/com/jsv/rest/UserWS.java
@@ -6,6 +6,7 @@ package com.jsv.rest;
  * Copyright(c) 2018 AXIS, LLC.
  */
 
+import com.jsv.rest.dal.UserDAL;
 import com.jsv.rest.model.User;
 import com.jsv.rest.util.Log;
 
@@ -13,6 +14,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import java.util.List;
 
 @Path("user") @Produces("application/json") public class UserWS {
     @GET @Path("/get") public User getUserByUserId() {
@@ -23,10 +25,16 @@ import javax.ws.rs.Produces;
         return null;
     }
 
-    @POST @Path("/login") public boolean login() {
+    @POST @Path("/login") public boolean login(User user) {
         Log.log("Start login");
         boolean success = false;
         //Body
+        Log.log(user.toString());
+        List userList = UserDAL.getUserByUserNameAndPassword(user);
+        Log.log("size=" + userList.size());
+        if (userList != null && !userList.isEmpty()) {
+            success = true;
+        }
 
         Log.log("End login");
         return success;
