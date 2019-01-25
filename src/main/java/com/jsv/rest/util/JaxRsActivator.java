e4035b7bf8fa1dc7e20f13cb1fc9af39c47b5daa
Indika Gunawardana <indika.gunawardana@gmail.com>
2019-01-25 09:44:25 +0530

    Sets cors filters

--- src/main/java/com/jsv/rest/util/JaxRsActivator.java
+++ src/main/java/com/jsv/rest/util/JaxRsActivator.java
@@ -6,9 +6,38 @@ package com.jsv.rest.util;
  * Copyright(c) 2018 AXIS, LLC.
  */
 
+import com.jsv.rest.GroupWS;
+import com.jsv.rest.ServiceWS;
+import com.jsv.rest.UserWS;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/rest")
 public class JaxRsActivator extends Application {
+    private Set<Object> singletons = new HashSet<Object>();
+    private HashSet<Class<?>> classes = new HashSet<Class<?>>();
+
+    public JaxRsActivator() {
+        CorsFilter corsFilter = new CorsFilter();
+        corsFilter.getAllowedOrigins().add("*");
+        corsFilter.setAllowedMethods("OPTIONS, GET, POST, DELETE, PUT, PATCH");
+        singletons.add(corsFilter);
+
+        classes.add(UserWS.class);
+        classes.add(ServiceWS.class);
+        classes.add(GroupWS.class);
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+
+    @Override
+    public HashSet<Class<?>> getClasses() {
+        return classes;
+    }
 }
