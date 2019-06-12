4767c6113ebb25c6d13aa481faf0550238fa1ce6
Indika Gunawardana <indika.gunawardana@gmail.com>
2019-06-13 05:19:45 +0530

    Make the method static.

--- src/main/java/com/jsv/rest/util/TokenGenerator.java
+++ src/main/java/com/jsv/rest/util/TokenGenerator.java
@@ -4,15 +4,14 @@ import java.security.SecureRandom;
 
 public class TokenGenerator {
 
-    protected static SecureRandom random = new SecureRandom();
+    private static SecureRandom random = new SecureRandom();
 
     /**
      * Generate base 16 string.
      * @return base 16 string.
      */
-    public synchronized String generateToken() {
+    public static synchronized String generateToken() {
         long longToken = Math.abs( random.nextLong() );
-        String random = Long.toString( longToken, 16 );
-        return random;
+        return Long.toString( longToken, 16 );
     }
 }
